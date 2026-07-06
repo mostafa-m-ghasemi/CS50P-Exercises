0cb3fb8746a375276a30865d48ef2929bf9d82f2
Mostafa <mostafa.m.qasemi@gmail.com>
2026-07-06 11:21:30 +0330

    Add files via upload

---
+++ Week 01/house.py
@@ -0,0 +1,27 @@
+name = input("What is your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+# elif name == "Hermione":
+#     print("Gryffindor")
+# elif name == "Ron":
+#     print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+
+# mach case
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    # case "Hermione":
+    #     print("Gryffindor")
+    # case "Ron":
+    #     print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
